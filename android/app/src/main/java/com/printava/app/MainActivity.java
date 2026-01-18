64e4683a213060b6f0265102cca031193ce2a93e
souzana <souzana.hamze@gmail.com>
2026-01-18 13:31:39 +0300

    add mobile app config

---
+++ android/app/src/main/java/com/printava/app/MainActivity.java
@@ -0,0 +1,5 @@
+package com.printava.app;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.printava.app',
+  appName: 'print-flow',
+  webDir: 'dist'
+};
+
+export default config;
